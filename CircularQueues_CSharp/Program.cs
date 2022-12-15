134fdd9abca5897dd66d551d2ffa14af4f1f65e7
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-12-15 16:15:45 +0700

    add class Queues

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace CircularQueues_CSharp
 {
+    class Queues
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues()
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
